0e0af49aae73b1dcac099d2b0d512c1ee28539be
tiandaguang <daguang.tian@ucarinc.com>
2019-04-10 14:54:21 +0800

    first 使用 nacos作为配置中心引入配置

--- nacos-spring-boot-config/src/main/java/com/tdg/boot/NacosSpringBootConfigApplication.java
+++ nacos-spring-boot-config/src/main/java/com/tdg/boot/NacosSpringBootConfigApplication.java
@@ -1,9 +1,11 @@
-package com.tdg.boot.nacosspringbootconfig;
+package com.tdg.boot;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@NacosPropertySource(dataId = "example", autoRefreshed = true)
 public class NacosSpringBootConfigApplication {
 
 	public static void main(String[] args) {
