4001bc41f6f548cdffb422181c1c29de8f77ae37
tiandaguang <daguang.tian@ucarinc.com>
2019-04-10 13:51:57 +0800

    初始化项目

---
+++ nacos-spring-boot-config/src/main/java/com/tdg/boot/nacosspringbootconfig/NacosSpringBootConfigApplication.java
@@ -0,0 +1,13 @@
+package com.tdg.boot.nacosspringbootconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosSpringBootConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacosSpringBootConfigApplication.class, args);
+	}
+
+}
